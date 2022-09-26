8db5b09347de6fdb7a230dedd7a97a4f3fa576d6
Alexander <alex.wood1722@gmail.com>
2022-09-26 04:30:25 -0700

    Finished Navbar component

--- data_map/src/Components/Navbar.js
+++ data_map/src/Components/Navbar.js
@@ -1,5 +1,59 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './Navbar.css'
 
 export default function Navbar() {
-    return <h1>Navbar</h1>
+    const [mySidebar, setMySidebar] = useState(false)
+    const styles = {
+        width: mySidebar ? '250px' : '0'
+    }
+
+    function openMenu() {
+        if (!mySidebar) {
+            setMySidebar(prevState => !prevState)
+        }
+    }
+
+    function closeMenu() {
+        if (mySidebar) {
+            setMySidebar(prevState => !prevState)
+        }
+    }
+
+    return (
+        <>
+            <header>
+                <nav className='nav'>
+                    <form className='searchBar'>
+                        <input 
+                            type='search'
+                            placeholder='Search for state or city...'
+                        />
+                        <button type='submit'></button>
+                    </form>
+                    <div style={styles} className='sidebar'>
+                        <a href="javascript:void(0)" className="closebtn" onClick={closeMenu}>&times;</a>
+                        <a href="#">About</a>
+                        <a href="#">Services</a>
+                        <a href="#">Clients</a>
+                        <a href="#">Contact</a>
+                    </div>
+                    <button className='Menu' onClick={openMenu}>
+                        <svg className='hamburgerMenu' width='40' height='40'>
+                            <rect width="100" height="8"></rect>
+                            <rect y="14" width="100" height="8"></rect>
+                            <rect y="28" width="100" height="8"></rect>
+                        </svg>
+                    </button>
+                    <div className='menuList'>
+                        <ul>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Services</a></li>
+                            <li><a href="#">Clients</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div>
+                </nav>
+            </header>
+        </>
+    )
 }
